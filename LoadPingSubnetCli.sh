877b39320f2c1a392c20d93fa4514a46aa1b4494
Dave Thelen <dt@arista.com>
2016-02-02 20:46:16 -0800

    changing permissions

--- LoadPingSubnetCli.sh
+++ LoadPingSubnetCli.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 cp /mnt/flash/PingSubnetCli.py /usr/lib/python2.7/site-packages/CliPlugin/
+chmod 644 /usr/lib/python2.7/site-packages/CliPlugin/PingSubnetCli.py
+pip install netaddr
