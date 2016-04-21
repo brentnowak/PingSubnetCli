c0169228d94c98ccc26fddf72064e0d8d582cb70
Brent Nowak <brent613@gmail.com>
2016-04-21 13:11:11 -0700

    update to use local copy of netaddr

--- LoadPingSubnetCli.sh
+++ LoadPingSubnetCli.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cp /mnt/flash/PingSubnetCli.py /usr/lib/python2.7/site-packages/CliPlugin/
 chmod 644 /usr/lib/python2.7/site-packages/CliPlugin/PingSubnetCli.py
-pip install netaddr
+cd /mnt/flash/netaddr-0.7.18
+sudo python setup.py install
